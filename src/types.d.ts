651a7982b528416de3a9740904f26c02da84cfcf
John Forster <john.forster@makers.tech>
2024-09-24 12:13:00 +0100

    Add helpful comments to config files

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,5 @@
 // This file tells vscode to treat .svg and .png files as strings,
-// preventing it from giving an error when importing them in components
+// preventing errors when importing them in components.
 declare module "*.svg" {
   const content: string;
   export default content;
